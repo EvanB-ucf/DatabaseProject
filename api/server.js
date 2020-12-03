b9602ee3415b926cb3f449d88d96d3c8376a936f
MelanieBrady <melaniebrady@knights.ucf.edu>
2020-12-03 18:51:37 -0500

    Updated to fix Unexpected token export error

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
-var mysql = require('mysql')
-export var connection = mysql.createConnection({
+var mysql = require('mysql');
+var connection = mysql.createConnection({
   host: 'databaseprojectcop4710.c3vnxknml348.us-east-1.rds.amazonaws.com',
   user: 'COP4710_Group3',
   password: 'garbageTier56',
   database: 'Main',
   port: 3306
-})
+});
+
+module.export = { connection }; 
