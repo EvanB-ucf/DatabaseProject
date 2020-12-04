6ee9062947dc821e2638037a8996d62bc1060c54
Kolbe <waterbomb58@gmail.com>
2020-12-03 17:09:18 -0800

    server.js export changed formatting

--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
 var mysql = require('mysql')
-var connection = mysql.createConnection({
+module.export  = mysql.createConnection({
   host: 'databaseprojectcop4710.c3vnxknml348.us-east-1.rds.amazonaws.com',
   user: 'COP4710_Group3',
   password: 'garbageTier56',
   database: 'Main',
   port: 3306
 })
-module.export = {connection}
