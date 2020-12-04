d2cf915ccadbb34ac0ddfb0678c3f6b7d58c727c
Andrew Borg <andrewborg1126@gmail.com>
2020-12-04 14:52:29 -0600

    Replaced Hello World on Event and Add pages with hello from node server.

--- api/server.js
+++ api/server.js
@@ -1,4 +1,8 @@
 var mysql = require('mysql')
+const express = require('express')
+const port = 5000;
+var cors = require("cors")
+const app = express()
 var connection = mysql.createConnection({
   host: 'databaseprojectcop4710.c3vnxknml348.us-east-1.rds.amazonaws.com',
   user: 'COP4710_Group3',
@@ -7,3 +11,14 @@ var connection = mysql.createConnection({
   port: 3306
 })
 module.export = {connection}
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World! (from the node.js server)');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
--- web/src/pages/add.js
+++ web/src/pages/add.js
@@ -1,11 +1,30 @@
 import React from "react";
+const server = "http://localhost:5000";
 
 export default class Add extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {helloResponseFromServer: ""};
+    }
+
+    askNodeServerForHello() {
+        fetch(server + "/", {
+            method: 'get'
+        })
+        .then(res => res.text())
+        .then(res => this.setState({helloResponseFromServer: res}))
+        .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.askNodeServerForHello();
+    }
+
     render() {
         // PUT HTML WITHIN RETURN
         return (
             <div>
-                <p>Hello World</p>
+                <p>{this.state.helloResponseFromServer}</p>
             </div>
         )
     }
