ff5023fd0290ef1b48eb5e8a3c67cd5f1d7e8fec
Kolbe <waterbomb58@gmail.com>
2020-12-03 16:15:16 -0800

    fixing export issue

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 var mysql = require('mysql')
-export var connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: 'databaseprojectcop4710.c3vnxknml348.us-east-1.rds.amazonaws.com',
   user: 'COP4710_Group3',
   password: 'garbageTier56',
   database: 'Main',
   port: 3306
 })
+module.export = {connection}
